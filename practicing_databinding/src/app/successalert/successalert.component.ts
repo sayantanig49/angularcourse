803e5347d697c8ea0870cad32470f4e7879c9e68
sayantanig49 <sayantanig49@gmail.com>
2023-11-04 17:49:24 +0530

    Practicing angular data binding

---
+++ practicing_databinding/src/app/successalert/successalert.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-successalert',
+  templateUrl: './successalert.component.html',
+  styleUrls: ['./successalert.component.css']
+})
+export class SuccessalertComponent {
+  user = 'TestUser';
+
+  getUser(){
+    return this.user;
+  }
+}
